c91fb763e7a2e6fd92052ee51f1c56a0de676f03
Yavuz Gumustepe <109815237+trygms@users.noreply.github.com>
2022-08-16 02:58:08 +0300

    operators

---
+++ conditions.js
@@ -0,0 +1,21 @@
+let say1 = +prompt("1.Sayı:");
+let say2 = +prompt("2.Sayı:");
+let islem = prompt(`"islem yazınız: +, -, *, /"`);
+let sonuc;
+//* 1.Yontem
+if (islem == '+') {
+  sonuc=say1+say2;
+}
+else if (islem == '-') {
+  sonuc=say1-say2;
+}
+else if (islem == '*') {
+  sonuc=say1*say2;
+}
+else if (islem == '/') {
+  sonuc=say1/say2;
+}
+else {
+console.log("Dort islemden birisini giriniz");
+}
+console.log(`Sonuç: ${sonuc} dir`);
